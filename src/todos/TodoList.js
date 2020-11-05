62b047993c822f33f3eaa0dd23b75971c165e588
Camilo Gomez <camilogomezmusic@gmail.com>
2020-11-04 19:40:25 -0500

    Add server. Update reducers and thunks to work with the DB

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TodoListItem from './TodoListItem';
 import { connect } from 'react-redux';
-import { removeTodo, markTodoAsCompleted } from './actions';
-import { displayAlert } from './thunks';
+import { loadTodos, removeTodoRequest, markTodoAsCompletedRequest } from './thunks';
 import './TodoList.css';
 
-const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
-  <div className="list-wrapper">
-    {todos.map( (todo, key) => 
-      <TodoListItem 
-        key={key} 
-        todo={todo} 
-        onRemovePressed={onRemovePressed} 
-        onCompletedPressed={onCompletedPressed}
-      />)}
-  </div>
-);
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos }) => {
+  useEffect(() => {
+    startLoadingTodos();
+  }, [])
+  
+  
+  const loadingMessage = <div>Loading todos...</div>
+  const content =  (
+    <div className="list-wrapper">
+      {todos.map( (todo, key) => 
+        <TodoListItem 
+          key={key} 
+          todo={todo} 
+          onRemovePressed={onRemovePressed} 
+          onCompletedPressed={onCompletedPressed}
+        />)}
+    </div>
+  );
+
+  return isLoading ? loadingMessage : content;
+}
+
 
 const mapStateToProps = state => ({
+  isLoading: state.isLoading,
   todos: state.todos,
 });
 
 const mapDispatchToProps = dispatch => ({
-  onRemovePressed: text => dispatch(removeTodo(text)),
-  onCompletedPressed: text => dispatch(markTodoAsCompleted(text)),
+  onRemovePressed: id => dispatch(removeTodoRequest(id)),
+  onCompletedPressed: id => dispatch(markTodoAsCompletedRequest(id)),
+  startLoadingTodos: () => dispatch(loadTodos())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
